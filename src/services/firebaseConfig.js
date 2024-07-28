bb709004b87c4a73280dc2027729d8703410e251
Pragathish <pragarajesh779jd@gmail.com>
2024-07-28 21:26:49 +0530

    touch ups

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0HgyfMsxpZBkecriw-aDCKMqLriweWh0",
-  authDomain: "auth-dev-3a10e.firebaseapp.com",
-  projectId: "auth-dev-3a10e",
-  storageBucket: "auth-dev-3a10e.appspot.com",
-  messagingSenderId: "1005205820678",
-  appId: "1:1005205820678:web:8a382c263496998fc6fa87"
+  apiKey: process.env.FIREBASE_API_KEY, //"AIzaSyB0HgyfMsxpZBkecriw-aDCKMqLriweWh0",
+  authDomain: process.env.FIREBASE_AUTHDOMAIN, //"auth-dev-3a10e.firebaseapp.com",
+  projectId: process.env.FIREBASE_PROJECT_ID,//"auth-dev-3a10e",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,//"auth-dev-3a10e.appspot.com",
+  messagingSenderId: process.env.FIREBASE_MESSAGE_ID,//"1005205820678",
+  appId: process.env.FIREBASE_APP_ID,//"1:1005205820678:web:8a382c263496998fc6fa87"
 };
 
 const app = initializeApp(firebaseConfig);
